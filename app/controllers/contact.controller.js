a2abb4851847f43a54c1bc80a3f88adc13d4e2b2
owen <owen@emailaddress.com>
2022-09-23 14:26:03 +0700

    Cai dat xu ly loi

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,6 +1,6 @@
-exports.create = (req, res ) => {
-    res.send({ message: " create handler " }) ;
- } ;
+exports.create = (req, res) => {
+    res.send({ message: "create handler" }) ;
+ };
  exports.findAll = (req, res) => {
     res.send({ message: "findAll handler" }) ;
  } ;
@@ -10,12 +10,11 @@ exports.create = (req, res ) => {
  exports.update = (req, res ) => {
     res.send({ message: "update handler" }) ;
 } ;
-exports.delete(req , res ) = > {
+exports.delete = (req, res ) => {
     res.send({ message: "delete handler" } ) ;
 } ;
-               =
-exports.deleteAll = ( req , res ) => {
-    res.send({ message: "deleteAll handler" } ) ;
+exports.deleteAll = (req, res ) => {
+    res.send({ message: "deleteAll handler" }) ;
 };
  exports.findAllFavorite = (req, res ) => {
     res.send({ message: "findAllFavorite handler" }) ;
